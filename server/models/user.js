188aa98b3d5c1fc29798719ffd7a817af973245d
jh.park <hongzzi.dev@gmail.com>
2019-03-25 16:30:00 +0900

    Success Join, Login, Id_check, getUsers, getUser

--- server/models/user.js
+++ server/models/user.js
@@ -8,7 +8,9 @@ var userSchema = new Schema({
         required: true,
         index: true,
         match:/([A-Z]|[a-z]|[가-힣]|[0-9])\w+/,
-        unique: true },
+        unique: true
+        //minlength:, maxlength:
+    },
     password: {
         type: String,
         valid: [
@@ -28,6 +30,42 @@ userSchema.statics.create = function(payload) {
     const user = new this(payload);
     //return promise
     return user.save();
+};
+
+// Find All
+userSchema.statics.findAll = function () {
+    // return promise
+    // V4부터 exec() 필요없음
+    return this.find({});
+};
+
+// Find One by userID
+userSchema.statics.findOneByUserid = function (userid) {
+    return this.findOne({ userid });
+};
+
+// id Check
+userSchema.statics.checkId = function (userid) {
+
+    const countQuery = this.where({ userid }).countDocuments();
+    console.log(countQuery);
+    return countQuery;
+};
+
+// id Check
+userSchema.statics.checkAuth = function (userid) {
+    return this.countDocuments({ userid });
+};
+
+// Update by todoid
+userSchema.statics.updateByUserid = function (userid, payload) {
+    // { new: true }: return the modified document rather than the original. defaults to false
+    return this.findOneAndUpdate({ userid }, payload, { new: true, useFindAndModify:false });
+};
+
+// Delete by todoid
+userSchema.statics.deleteByUserid = function (userid) {
+    return this.remove({ userid });
 };
 
 
--- server/routes/route.js
+++ server/routes/route.js
@@ -10,47 +10,45 @@ router.get('/',(req,res) => {
 
 });
 
-// 회원가입
+// 회원가입 (성공)
 router.post('/join', (req,res) => {
 		User.create(req.body)
 			.then(user => res.send(user))
 			.catch(err => res.status(500).send(err));
 });
 
-// 로그인
+// 아이디 중복확인 (성공)
+router.post('/check/id',(req,res) => {
+	User.countDocuments({userid:req.body.userid})
+		.then((count) => {
+    	    if (count!=0) return res.status(404).send({ count : 1 });
+    		res.send({ count : 0 });
+		})
+        .catch(err => res.status(500).send(err));
+});
+
+// 로그인 (성공) ===> user 비어있을 때 값처리 다시 한 번 해봐야함
 router.post('/login', (req, res) => {
-    User.find({"userid": req.params.userid, "password": req.params.password})
+    console.log(req.body);
+    User.find({userid: req.body.userid, password: req.body.password})
         .then((users) => {
             if (!users) return res.status(404).send({err: 'User not found'});
-            res.send('find successfully!'+users);
+            res.send(users);
         })
         .catch(err => res.status(500).send(err));
 });
 
-// 아이디 중복확인
-router.post('/check/id',(req,res) => {
 
-	User.countDocuments({"userid": req.params.userid}, (err, count) => {
-        if (err) {
-            res.send(err);
-            return;
-        }
-        res.json({count: count});	//반환값 0/1
-    })
-});
-
-
-
-// 개인정보
+// 개인정보 (성공)
 router.post('/get/user',(req,res) => {
-        User.findOne({userid: req.params.userid})
+    console.log(req.body);
+        User.findOne({userid: req.body.userid})
             .then(users => res.send(users))
             .catch(err => res.status(500).send(err));
     }
 )
 
-
-// 모든 유저 정보
+// 모든 유저 정보 (성공)
 router.get('/user', (req, res) => {
 	// GET ALL User
 	User.find(function(err, users) {
@@ -58,21 +56,6 @@ router.get('/user', (req, res) => {
 		res.json(users);
 	})
 });
-
-// 특정 유저 정보
-router.get('/user/get/:userid',(req, res) => {
-	// GET One user
-	User.findOneByUserid(req.params.userid)
-		.then((users) => {
-			if(!users) return res.status(404).send({ err: 'User not found'});
-			res.send('findOne successfully: ${user}');
-		})
-		.catch(err => res.status(500).send(err));
-	/*	User.find({ name: req.params.name }, (err, user) => {
-		res.render('main', { user: user} );
-	}); */
-});
-
 
 router.put('/user/update/:userid', (req, res) => {
     // UPDATE user
@@ -83,7 +66,7 @@ router.put('/user/update/:userid', (req, res) => {
 
 router.delete('/user/delete/:userid', (req,res) => {
 	// Delete User
-	User.remove(req.params.id)
+	User.remove(req.params.userid)
 		.then(()=> res.status(200))
 		.catch(err => res.status(500).send(err));
 });
